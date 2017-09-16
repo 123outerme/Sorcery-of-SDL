0e4b547e504f2347fd279957535c55c9be9e0290
Stephen Policelli <123outerme@outlook.com>
2017-09-15 23:13:48 -0400

    Fixed version.h and changelog

--- version.h
+++ version.h
@@ -15,16 +15,16 @@
 	#define MAJOR  0
 	#define MINOR  1
 	#define BUILD  1
-	#define REVISION  2
+	#define REVISION  0
 	
 	//Miscellaneous Version Types
-	#define BUILDS_COUNT  159
-	#define RC_FILEVERSION 0,1,1,2
-	#define RC_FILEVERSION_STRING "0, 1, 1, 2\0"
-	#define FULLVERSION_STRING  "0.1.1.2"
+	#define BUILDS_COUNT  116
+	#define RC_FILEVERSION 0,1,1,0
+	#define RC_FILEVERSION_STRING "0, 1, 1, 0\0"
+	#define FULLVERSION_STRING  "0.1.1.0"
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	#define BUILD_HISTORY  2
+	#define BUILD_HISTORY  0
 	
 
 #endif //VERSION_H
