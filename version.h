cc3a38b941688357d6e163b7bb187d1413f174a7
Stephen Policelli <123outerme@outlook.com>
2017-10-14 12:38:27 -0400

    Fixed version.h. Man I hate autoversioning in C::B

--- version.h
+++ version.h
@@ -2,7 +2,7 @@
 #define VERSION_H
 
 	//Date Version Types
-	#define DATE "10"
+	#define DATE "14"
 	#define MONTH "10"
 	#define YEAR "2017"
 	#define UBUNTU_VERSION_STYLE  "17.10"
@@ -14,14 +14,14 @@
 	//Standard Version Type
 	#define MAJOR  1
 	#define MINOR  0
-	#define BUILD  2
+	#define BUILD  3
 	#define REVISION  9
 	
 	//Miscellaneous Version Types
-	#define BUILDS_COUNT  326
-	#define RC_FILEVERSION 1,0,2,9
-	#define RC_FILEVERSION_STRING "1, 0, 2, 9\0"
-	#define FULLVERSION_STRING  "1.0.2.9"
+	#define BUILDS_COUNT  386
+	#define RC_FILEVERSION 1,0,3,9
+	#define RC_FILEVERSION_STRING "1, 0, 3, 9\0"
+	#define FULLVERSION_STRING  "1.0.3.9"
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	#define BUILD_HISTORY  0
