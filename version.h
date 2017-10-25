4bd580c356dd78fb8a8bf0a471462310dd4d86e2
Stephen Policelli <123outerme@outlook.com>
2017-10-25 15:13:09 -0400

    Changed version number to 1.1

--- version.h
+++ version.h
@@ -2,26 +2,26 @@
 #define VERSION_H
 
 	//Date Version Types
-	#define DATE "14"
+	#define DATE "25"
 	#define MONTH "10"
 	#define YEAR "2017"
 	#define UBUNTU_VERSION_STYLE  "17.10"
 	
 	//Software Status
 	#define STATUS  ""
-	#define STATUS_SHORT  " "
+	#define STATUS_SHORT  ""
 	
 	//Standard Version Type
 	#define MAJOR  1
-	#define MINOR  0
-	#define BUILD  3
-	#define REVISION  9
+	#define MINOR  1
+	#define BUILD  0
+	#define REVISION  0
 	
 	//Miscellaneous Version Types
-	#define BUILDS_COUNT  386
-	#define RC_FILEVERSION 1,0,3,9
-	#define RC_FILEVERSION_STRING "1, 0, 3, 9\0"
-	#define FULLVERSION_STRING  "1.0.3.9"
+	#define BUILDS_COUNT  434
+	#define RC_FILEVERSION 1,1,0,0
+	#define RC_FILEVERSION_STRING "1, 1, 0, 0\0"
+	#define FULLVERSION_STRING  "1.1.0.0"
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	#define BUILD_HISTORY  0
